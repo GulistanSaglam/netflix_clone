18368de5362d18c47a64078a70c7e1c7740d80f9
Gülistan Sağlam <99056956+GulistanSaglam@users.noreply.github.com>
2024-04-03 02:01:14 +0300

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -6,4 +6,4 @@ const instance = axios.create({
 
 export default instance;
 
-// http://api.themoviedb.org/3/trending/all/week?api_key=44648c87bee16eb91553f608c5a6ed48&language=en-US
+// http://api.themoviedb.org/3/trending/all/week?api_key=YOUR API KEY8&language=en-US
